e13391dac99caac463541450d9bcdf90f21d171f
jondeh <dehlinjd@gmail.com>
2020-07-21 20:14:34 -0600

    removed unused variables

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
+// import Header from './components/Header/Header';
 import Body from './components/Body/Body';
-import Footer from './components/Footer/Footer';
+// import Footer from './components/Footer/Footer';
 
 function App() {
 
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useEffect, useRef} from 'react';
 import {TweenMax} from 'gsap';
 import './LandingPage.css';
 import {FaGithub, FaLinkedin, FaArrowDown} from 'react-icons/fa';
